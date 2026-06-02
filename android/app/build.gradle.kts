41810695e0fb41397769e8dbd072de9ec98635cb
Peter Neal <roguenealer@gmail.com>
2026-06-02 19:34:26 +0000

    v1.7.0: Add Household Food Planner (household size + target duration -> flags food that won't last)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kitchenforge.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.6.0"
+        versionCode = 11
+        versionName = "1.7.0"
     }
 
     signingConfigs {
